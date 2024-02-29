0548d8c421c7a7b645983201e700e0fa30245d01
prempunith <104521021+pprempunith@users.noreply.github.com>
2024-02-29 23:44:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).keydown( function(event){
     $("h1").text(event.key);
 })
+
+$("button").on("click", function(){
+    $("h1").toggle();
+})
+
+$("h1").off("mouseover", function(){
+    $("h1").css("color", "purple");
+})  
+
+$
